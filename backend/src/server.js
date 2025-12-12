1622644340a2dfeac1ad5237ec06d1a1f932526d
Bishnu Mukherjee <jerry.bs12345@gmail.com>
2025-12-12 21:54:30 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,11 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// 1. RECREATE __dirname FOR ES MODULES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -7,4 +13,19 @@ app.get("/health", (req, res) => {
     res.status(200).json({msg : "api is up and running"});
 })
 
-app.listen(ENV.PORT, () => console.log(`server is running on port: ${ENV.PORT}`))
+app.get("/books", (req, res) => {
+    res.status(200).json({msg : "this is the books endpoint"});
+})
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+    // 2. PATH FIX: Go up 2 levels (../../) to escape 'src' and 'backend'
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    // 3. EXPRESS 5 FIX: Use '*splat' for the wildcard
+    app.get("*splat", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    })
+}
+
+app.listen(ENV.PORT, () => console.log(`server is running on port: ${ENV.PORT}`));
